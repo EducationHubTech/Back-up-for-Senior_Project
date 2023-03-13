c6fd7ee19a0f2c8c4025db0d1ee5c22acf1f37d9
Yoseph Negash <yosphn22@gmail.com>
2023-03-13 07:16:46 +0300

    frist commit

---
+++ DB/on_the_go_incident_reporter.sql
@@ -0,0 +1,186 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 13, 2023 at 03:55 AM
+-- Server version: 10.4.13-MariaDB
+-- PHP Version: 7.4.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `on_the_go incident reporter`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `admin_id` varchar(50) CHARACTER SET latin1 NOT NULL,
+  `admin_pass` varchar(50) CHARACTER SET latin1 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `complaint`
+--
+
+CREATE TABLE `complaint` (
+  `c_id` int(11) NOT NULL,
+  `a_no` bigint(12) NOT NULL,
+  `location` varchar(50) NOT NULL,
+  `type_crime` varchar(50) NOT NULL,
+  `d_o_c` date NOT NULL,
+  `r_t` varchar(50) NOT NULL DEFAULT current_timestamp(),
+  `description` varchar(7000) NOT NULL,
+  `inc_status` varchar(50) DEFAULT 'Unassigned',
+  `pol_status` varchar(50) DEFAULT 'null',
+  `p_id` varchar(50) DEFAULT 'Null'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `handler`
+--
+
+CREATE TABLE `handler` (
+  `h_id` varchar(50) CHARACTER SET latin1 NOT NULL,
+  `h_pass` varchar(50) CHARACTER SET latin1 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `police`
+--
+
+CREATE TABLE `police` (
+  `p_name` varchar(50) NOT NULL,
+  `p_id` varchar(50) NOT NULL,
+  `spec` varchar(50) NOT NULL,
+  `location` varchar(50) NOT NULL,
+  `p_pass` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `taker`
+--
+
+CREATE TABLE `taker` (
+  `t_id` varchar(50) CHARACTER SET latin1 NOT NULL,
+  `t_pass` varchar(50) CHARACTER SET latin1 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `update_case`
+--
+
+CREATE TABLE `update_case` (
+  `c_id` int(11) NOT NULL,
+  `d_o_u` timestamp NOT NULL DEFAULT current_timestamp(),
+  `case_update` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `u_name` varchar(50) NOT NULL,
+  `u_id` varchar(50) NOT NULL,
+  `u_pass` varchar(50) NOT NULL,
+  `u_addr` varchar(100) NOT NULL,
+  `a_no` bigint(12) NOT NULL,
+  `gen` varchar(15) NOT NULL,
+  `mob` bigint(13) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`u_name`, `u_id`, `u_pass`, `u_addr`, `a_no`, `gen`, `mob`) VALUES
+('Yoseph Negash', 'yosephn22@gmail.com', '123456', 'Addis Ababa', 123214521452, 'Male', 947461118);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`admin_id`);
+
+--
+-- Indexes for table `complaint`
+--
+ALTER TABLE `complaint`
+  ADD PRIMARY KEY (`c_id`);
+
+--
+-- Indexes for table `handler`
+--
+ALTER TABLE `handler`
+  ADD PRIMARY KEY (`h_id`);
+
+--
+-- Indexes for table `police`
+--
+ALTER TABLE `police`
+  ADD PRIMARY KEY (`p_id`);
+
+--
+-- Indexes for table `taker`
+--
+ALTER TABLE `taker`
+  ADD PRIMARY KEY (`t_id`);
+
+--
+-- Indexes for table `update_case`
+--
+ALTER TABLE `update_case`
+  ADD UNIQUE KEY `d_o_u` (`d_o_u`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`a_no`),
+  ADD UNIQUE KEY `u_id` (`u_id`),
+  ADD UNIQUE KEY `mob` (`mob`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `complaint`
+--
+ALTER TABLE `complaint`
+  MODIFY `c_id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
