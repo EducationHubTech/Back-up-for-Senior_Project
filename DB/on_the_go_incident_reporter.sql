aa7a30ec4d10f50b2460ed0b0d00f92ad5720dc8
Yoseph Negash <yosphn22@gmail.com>
2023-03-14 18:55:28 +0300

    taker part modified

--- DB/on_the_go_incident_reporter.sql
+++ DB/on_the_go_incident_reporter.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 13, 2023 at 02:40 PM
+-- Generation Time: Mar 14, 2023 at 04:54 PM
 -- Server version: 10.4.13-MariaDB
 -- PHP Version: 7.4.8
 
@@ -40,16 +40,28 @@ CREATE TABLE `admin` (
 
 CREATE TABLE `complaint` (
   `c_id` int(11) NOT NULL,
-  `a_no` bigint(12) NOT NULL,
+  `id_no` bigint(12) NOT NULL,
   `location` varchar(50) NOT NULL,
   `type_crime` varchar(50) NOT NULL,
   `d_o_c` date NOT NULL,
-  `r_t` varchar(50) NOT NULL DEFAULT current_timestamp(),
+  `repo_time_and_date` varchar(50) NOT NULL DEFAULT current_timestamp(),
   `description` varchar(7000) NOT NULL,
   `inc_status` varchar(50) DEFAULT 'Unassigned',
   `pol_status` varchar(50) DEFAULT 'null',
   `p_id` varchar(50) DEFAULT 'Null'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `complaint`
+--
+
+INSERT INTO `complaint` (`c_id`, `id_no`, `location`, `type_crime`, `d_o_c`, `repo_time_and_date`, `description`, `inc_status`, `pol_status`, `p_id`) VALUES
+(1, 13577, 'Addis Ketema', 'Robbery', '2023-03-13', '2023-03-13 17:24:00', 'some one robbed the house!!', 'Unassigned', 'null', 'Null'),
+(2, 13577, 'Addis Ketema', 'Robbery', '2023-03-13', '2023-03-13 17:31:41', 'some one robbed the house!!', 'Unassigned', 'null', 'Null'),
+(3, 22345, 'Akaki-Kality', 'Theft', '2023-03-13', '2023-03-13 19:00:58', 'he ...', 'Unassigned', 'null', 'Null'),
+(4, 22345, 'Akaki-Kality', 'Theft', '2023-03-13', '2023-03-13 19:01:38', 'he ...', 'Unassigned', 'null', 'Null'),
+(5, 22345, 'Addis Ketema', 'Pick Pocket', '2023-03-06', '2023-03-13 19:02:22', 'put it back!', 'Unassigned', 'null', 'Null'),
+(6, 32232, 'Akaki-Kality', 'Theft', '2023-03-08', '2023-03-14 18:31:43', 'hey my name is sefanit what about you!', 'Unassigned', 'null', 'Null');
 
 -- --------------------------------------------------------
 
@@ -83,9 +95,17 @@ CREATE TABLE `police` (
 --
 
 CREATE TABLE `taker` (
-  `t_id` varchar(50) CHARACTER SET latin1 NOT NULL,
-  `t_pass` varchar(50) CHARACTER SET latin1 NOT NULL
+  `T_id` varchar(50) CHARACTER SET latin1 NOT NULL,
+  `T_pass` varchar(50) CHARACTER SET latin1 NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `taker`
+--
+
+INSERT INTO `taker` (`T_id`, `T_pass`) VALUES
+('manuhe12@gmail.com', '1256'),
+('yosephn22@gmail.com', '1234');
 
 -- --------------------------------------------------------
 
@@ -122,7 +142,7 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`u_name`, `u_id`, `u_pass`, `sub`, `woreda`, `id_no`, `gen`, `mob`) VALUES
 ('Yoseph Negash', 'yosephn22@gmail.com', '12@3456', '', 0, 0, '12345', 0),
-('mahlet worku', 'mahlet@34552', 'mahi12', 'Lideta', 13, 13577, 'Female', 251345674833),
+('mahlet worku', 'mahlet13@gmail.com', 'mahi12', 'Lideta', 13, 13577, 'Female', 251345674833),
 ('girum seifu', 'girum@3452', '222333', 'Lemi Kura', 4, 22345, 'Male', 251945678689),
 ('ahlam', 'ahlam42@gmail.com', '12@3456', 'Kolfe Keranio', 14, 32145, 'Female', 254987654234),
 ('sefanit negash', 'sefanit@123', '1233456', 'Addis Ketema', 8, 32232, 'Female', 251934567890),
@@ -160,7 +180,7 @@ ALTER TABLE `police`
 -- Indexes for table `taker`
 --
 ALTER TABLE `taker`
-  ADD PRIMARY KEY (`t_id`);
+  ADD PRIMARY KEY (`T_id`);
 
 --
 -- Indexes for table `update_case`
@@ -184,7 +204,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `complaint`
 --
 ALTER TABLE `complaint`
-  MODIFY `c_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `c_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
---
+++ TakerHome.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table {
+    border-collapse: collapse;
+    width: 100%;
+}
+
+th, td {
+    padding: 8px;
+    text-align: left;
+    border-bottom: 1px solid #000;
+}
+
+tr:hover {
+    background-color: #f5f5f5;
+}
+
+tr:nth-child(even) {
+    background-color: #f2f2f2;
+}
+
+.space {
+    margin-bottom: 16px;
+}
+</style>
+
+</head>
+<body style="background-color: #dfdfdf">
+	<nav  class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="home.php"><b> on_the-go incident reporter </b></a>
+    </div>
+    <div id="navbar" class="collapse navbar-collapse">
+      <ul class="nav navbar-nav">
+        <li ><a href="official_login.php">Official Login</a></li>
+        <li ><a href="Takerlogin.php">Taker Login</a></li>
+        <li class="active"><a href="TakerHome.php">Taker Home</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+      
+        <li><a href="Taker_logout.php">Logout &nbsp <i class="fa fa-sign-out" aria-hidden="true"></i></a></li>
+      </ul>
+    </div>
+  </div>
+ </nav>
+
+    <form style="margin-top: 7%; margin-left: 40%;" method="post">
+      <input type="text" name="cid" style="width: 250px; height: 30px; background-color:white;" placeholder="&nbsp Complaint Id" id="ciid" onfocusout="f1()" required>
+        <div>
+      <input class="btn btn-primary" type="submit" value="Search" name="s2" style="margin-top: 10px; margin-left: 11%;"> </br> </br>
+        </div>
+    </form>
+    
+    
+    
+
+    <div style="position: fixed;
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   height: 30px;
+   background-color: rgba(0,0,0,0.8);
+   color: white;
+   text-align: center;">
+  <h4 style="color: white;">&copy <b> on_the_go incident reporter | All Right Reserved</b></h4>
+</div>
+<?php
+    session_start();
+    if(!isset($_SESSION['x']))
+        header("location:Takerlogin.php");
+    
+    $conn=mysqli_connect("localhost","root","");
+   
+    if (!$conn) {
+      die("Connection failed: " . mysqli_connect_error());
+  }
+  mysqli_select_db($conn,"on_the_go incident reporter");
+  // Fetch all the complaints from the database
+  $sql = "SELECT c_id, type_crime, d_o_c,location, inc_status, p_id FROM complaint";
+  $result = mysqli_query($conn, $sql);
+  
+  // Check if there are any complaints in the database
+  if (mysqli_num_rows($result) > 0) {
+      // Start the table and output the header row
+      echo "<table>";
+      echo "<tr>
+      <th>Complaint ID</th>
+      <th>Type of Crime</th>
+      <th>Date of Crime</th>
+      <th>Location</th>
+      <th>Complaint Status</th>
+      <th>Police ID</th></tr>";
+  
+      // Loop through the result set and output each row as a table row
+      while ($row = mysqli_fetch_assoc($result)) {
+          echo "<tr>
+          <td>" . $row["c_id"] . "</td>
+          <td>" . $row["type_crime"] . "</td>
+          <td>" . $row["d_o_c"] . "</td>
+          <td>" . $row["location"] . "</td>
+          <td>" . $row["inc_status"] . "</td>
+          <td>" . $row["p_id"] . "</td>
+          </tr>";
+      }
+  
+      // End the table
+      echo "</table>";
+  } else {
+      // If there are no complaints in the database, output a message
+      echo "No complaints found.";
+  }
+  
+  // Close the database connection
+  mysqli_close($conn);
+  ?>
+	<title>Taker Homepage</title>
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+	<link href="http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+	
+    <script>
+     function f1()
+        {
+          var sta2=document.getElementById("ciid").value;
+          var x2=sta2.indexOf(' ');
+     if(sta2!="" && x2>=0)
+     {
+        document.getElementById("ciid").value="";
+        alert("Blank Field not Allowed");
+      }       
+}
+</script>
+
+ <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.4.js"></script>
+ <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+</body>
+</html>
